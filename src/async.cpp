c7bc13aacfe66292bab16323f005114753325263
Daisuke Bauza <144384874+daisuke1227@users.noreply.github.com>
2025-04-09 23:29:51 -0400

    I hope this works >~<

--- src/async.cpp
+++ src/async.cpp
@@ -4,99 +4,94 @@
 #define DEBUG_PROCESSING false
 
 void AsyncUILayer::handleKeypress(cocos2d::enumKeyCodes key, bool down) {
-	auto event = ExtendedCCKeyboardDispatcher::getCurrentEventInfo();
-	if (!event) {
-		return UILayer::handleKeypress(key, down);
-	}
+#ifdef GEODE_IS_MACOS
+    auto event = ExtendedCCKeyboardDispatcher::getCurrentEventInfo();
+    if (event) {
+        auto extendedInfo = static_cast<ExtendedCCEvent*>(event);
+        m_fields->m_lastTimestamp = extendedInfo->getTimestamp();
 
-	auto extendedInfo = static_cast<ExtendedCCEvent*>(event);
-	m_fields->m_lastTimestamp = extendedInfo->getTimestamp();
+        UILayer::handleKeypress(key, down);
+        static_cast<CustomGJBaseGameLayer*>(this->m_gameLayer)->fixUntimedInputs();
 
-	UILayer::handleKeypress(key, down);
-	static_cast<CustomGJBaseGameLayer*>(this->m_gameLayer)->fixUntimedInputs();
-
-	m_fields->m_lastTimestamp = 0ull;
+        m_fields->m_lastTimestamp = 0ull;
+        return;
+    }
+#endif
+    // Fallback for platforms that don't have keyboard events (like iOS)
+    UILayer::handleKeypress(key, down);
 }
 
 bool AsyncUILayer::ccTouchBegan(cocos2d::CCTouch* touch, cocos2d::CCEvent* event) {
-	if (!event) {
-		return UILayer::ccTouchBegan(touch, event);
-	}
+    if (!event) {
+        return UILayer::ccTouchBegan(touch, event);
+    }
 
-	auto extendedInfo = static_cast<ExtendedCCEvent*>(event);
-	m_fields->m_lastTimestamp = extendedInfo->getTimestamp();
+    auto extendedInfo = static_cast<ExtendedCCEvent*>(event);
+    m_fields->m_lastTimestamp = extendedInfo->getTimestamp();
 
-	auto r = UILayer::ccTouchBegan(touch, event);
-	static_cast<CustomGJBaseGameLayer*>(this->m_gameLayer)->fixUntimedInputs();
+    auto r = UILayer::ccTouchBegan(touch, event);
+    static_cast<CustomGJBaseGameLayer*>(this->m_gameLayer)->fixUntimedInputs();
 
-	m_fields->m_lastTimestamp = 0ull;
-
-	return r;
+    m_fields->m_lastTimestamp = 0ull;
+    return r;
 }
 
 void AsyncUILayer::ccTouchMoved(cocos2d::CCTouch* touch, cocos2d::CCEvent* event) {
-	if (!event) {
+    if (!event) {
 #ifdef GEODE_IS_ANDROID
-		if (this->m_inPlatformer) {
-			this->processUINodesTouch(
-				static_cast<GJUITouchEvent>(1), touch
-			);
-		}
-		return;
+        if (this->m_inPlatformer) {
+            this->processUINodesTouch(static_cast<GJUITouchEvent>(1), touch);
+        }
+        return;
 #else
-		return UILayer::ccTouchMoved(touch, event);
+        return UILayer::ccTouchMoved(touch, event);
 #endif
-	}
+    }
 
-	auto extendedInfo = static_cast<ExtendedCCEvent*>(event);
-	m_fields->m_lastTimestamp = extendedInfo->getTimestamp();
+    auto extendedInfo = static_cast<ExtendedCCEvent*>(event);
+    m_fields->m_lastTimestamp = extendedInfo->getTimestamp();
 
 #ifdef GEODE_IS_ANDROID
-	if (this->m_inPlatformer) {
-		this->processUINodesTouch(
-			static_cast<GJUITouchEvent>(1), touch
-		);
-	}
+    if (this->m_inPlatformer) {
+        this->processUINodesTouch(static_cast<GJUITouchEvent>(1), touch);
+    }
 #else
-	UILayer::ccTouchMoved(touch, event);
-	static_cast<CustomGJBaseGameLayer*>(this->m_gameLayer)->fixUntimedInputs();
+    UILayer::ccTouchMoved(touch, event);
+    static_cast<CustomGJBaseGameLayer*>(this->m_gameLayer)->fixUntimedInputs();
 #endif
 
-	m_fields->m_lastTimestamp = 0ull;
+    m_fields->m_lastTimestamp = 0ull;
 }
 
 void AsyncUILayer::ccTouchEnded(cocos2d::CCTouch* touch, cocos2d::CCEvent* event) {
-	if (!event) {
-		return UILayer::ccTouchEnded(touch, event);
-	}
+    if (!event) {
+        return UILayer::ccTouchEnded(touch, event);
+    }
 
-	auto extendedInfo = static_cast<ExtendedCCEvent*>(event);
-	m_fields->m_lastTimestamp = extendedInfo->getTimestamp();
+    auto extendedInfo = static_cast<ExtendedCCEvent*>(event);
+    m_fields->m_lastTimestamp = extendedInfo->getTimestamp();
 
-	UILayer::ccTouchEnded(touch, event);
-	static_cast<CustomGJBaseGameLayer*>(this->m_gameLayer)->fixUntimedInputs();
+    UILayer::ccTouchEnded(touch, event);
+    static_cast<CustomGJBaseGameLayer*>(this->m_gameLayer)->fixUntimedInputs();
 
-	m_fields->m_lastTimestamp = 0ull;
+    m_fields->m_lastTimestamp = 0ull;
 }
 
 #ifndef GEODE_IS_WINDOWS
 void AsyncUILayer::ccTouchCancelled(cocos2d::CCTouch* touch, cocos2d::CCEvent* event) {
-	// this event can't actually be called on anything but android/ios,
-	// so should be safe to just rewrite them
+    // On platforms like Android and iOS this event typically maps to a touch ending.
+    if (!event) {
+        return this->ccTouchEnded(touch, event);
+    }
 
-	if (!event) {
-		return this->ccTouchEnded(touch, event);
-	}
+    auto extendedInfo = static_cast<ExtendedCCEvent*>(event);
+    m_fields->m_lastTimestamp = extendedInfo->getTimestamp();
 
-	auto extendedInfo = static_cast<ExtendedCCEvent*>(event);
-	m_fields->m_lastTimestamp = extendedInfo->getTimestamp();
-
-	this->ccTouchEnded(touch, event);
-
-	m_fields->m_lastTimestamp = 0ull;
+    this->ccTouchEnded(touch, event);
+    m_fields->m_lastTimestamp = 0ull;
 }
 #endif
 
 std::uint64_t AsyncUILayer::getLastTimestamp() {
-	return m_fields->m_lastTimestamp;
+    return m_fields->m_lastTimestamp;
 }
--- src/input.cpp
+++ src/input.cpp
@@ -1,46 +1,202 @@
+#include "main.hpp"
+#include "async.hpp"
 #include "input.hpp"
 
-#define DEBUG_DISPATCH false
+#define DEBUG_STEPS false
 
-#ifndef GEODE_IS_IOS
-
-std::uint64_t ExtendedCCKeyboardDispatcher::g_lastTimestamp = 0ull;
-cocos2d::CCEvent* ExtendedCCKeyboardDispatcher::g_currentEventInfo = nullptr;
-
-void ExtendedCCKeyboardDispatcher::setTimestamp(std::uint64_t timestamp) {
-	g_lastTimestamp = timestamp;
-}
-
-cocos2d::CCEvent* ExtendedCCKeyboardDispatcher::getCurrentEventInfo() {
-	return g_currentEventInfo;
+#if DEBUG_STEPS
+// this one's me being lazy
+template <typename T>
+inline T* ptr_to_offset(void* base, unsigned int offset) {
+    return reinterpret_cast<T*>(reinterpret_cast<uintptr_t>(base) + offset);
 };
 
-bool ExtendedCCKeyboardDispatcher::dispatchKeyboardMSG(cocos2d::enumKeyCodes key, bool isKeyDown, bool isKeyRepeat) {
-#if DEBUG_DISPATCH
-	geode::log::debug("dispatch keyboard event with time {}", g_lastTimestamp);
+template <typename T>
+inline T get_from_offset(void* base, unsigned int offset) {
+    return *ptr_to_offset<T>(base, offset);
+};
 #endif
 
-	auto event = ExtendedCCEvent(g_lastTimestamp);
-	g_currentEventInfo = &event;
-
-	auto r = CCKeyboardDispatcher::dispatchKeyboardMSG(key, isKeyDown, isKeyRepeat);
-
-	g_currentEventInfo = nullptr;
-	g_lastTimestamp = 0ull;
-
-	return r;
+// Get timestamp with compatibility for custom keybinds.
+// On macOS we use keyboard event info; otherwise return 0.
+std::uint64_t getTimestampCompat() {
+#ifdef GEODE_IS_MACOS
+    auto event = ExtendedCCKeyboardDispatcher::getCurrentEventInfo();
+    if (event) {
+        auto extendedInfo = static_cast<ExtendedCCEvent*>(event);
+        return extendedInfo->getTimestamp();
+    }
+#endif
+    return 0;
 }
 
+#ifdef GEODE_IS_WINDOWS
+void CustomGJBaseGameLayer::queueButton_custom(int btnType, bool push, bool secondPlayer) {
+#else
+void CustomGJBaseGameLayer::queueButton(int btnType, bool push, bool secondPlayer) {
+#endif
+    // Workaround for passing arguments etc.
+    auto& fields = this->m_fields;
+
+#ifdef GEODE_IS_ANDROID
+    // If the player is currently in a frame, insert the input directly.
+    if (fields->m_inFrame) {
+        return GJBaseGameLayer::queueButton(btnType, push, secondPlayer);
+    }
 #endif
 
-std::uint64_t ExtendedCCTouchDispatcher::g_lastTimestamp = 0ull;
+    auto inputTimestamp = static_cast<AsyncUILayer*>(this->m_uiLayer)->getLastTimestamp();
+#ifdef GEODE_IS_MACOS
+    if (!inputTimestamp) {
+        inputTimestamp = getTimestampCompat();
+    }
+#endif
 
-void ExtendedCCTouchDispatcher::setTimestamp(std::uint64_t timestamp) {
-	g_lastTimestamp = timestamp;
+    auto timeRelativeBegin = fields->m_timeBeginMs;
+    auto currentTime = inputTimestamp - timeRelativeBegin;
+    if (inputTimestamp < timeRelativeBegin || !inputTimestamp || !timeRelativeBegin) {
+        // If time is not yet initialized, use zero.
+        currentTime = 0;
+    }
+
+    // Calculate the step, maintaining compatibility with the physics bypass.
+    auto inputOffset = fields->m_inputOffset;
+    if (fields->m_inputOffsetRand != 0) {
+        auto offsetMax = fields->m_inputOffsetRand;
+        auto randValue = static_cast<double>(rand()) / static_cast<double>(RAND_MAX);
+        auto randOffset = static_cast<std::int32_t>((offsetMax * 2) * randValue - offsetMax);
+        inputOffset += randOffset;
+    }
+
+    if (inputOffset < 0 && -inputOffset > currentTime) {
+        currentTime = 0;
+    } else {
+        currentTime += inputOffset;
+    }
+
+#if DEBUG_STEPS
+    geode::log::debug("queueing input type={} down={} p2={} at time {} (ts {} -> {})",
+        btnType, push, secondPlayer, currentTime, timeRelativeBegin, inputTimestamp);
+#endif
+
+    fields->m_timedCommands.push({
+        static_cast<PlayerButton>(btnType),
+        push,
+        secondPlayer,
+        static_cast<std::int32_t>(currentTime)
+    });
 }
 
-void ExtendedCCTouchDispatcher::touches(cocos2d::CCSet* touches, cocos2d::CCEvent* event, unsigned int index) {
-	auto extEvent = ExtendedCCEvent(g_lastTimestamp);
-	CCTouchDispatcher::touches(touches, &extEvent, index);
-	g_lastTimestamp = 0ull;
+void CustomGJBaseGameLayer::resetLevelVariables() {
+    GJBaseGameLayer::resetLevelVariables();
+    auto& fields = m_fields;
+    fields->m_timeBeginMs = 0;
+    fields->m_timeOffset = 0.0;
+    fields->m_timedCommands = {};
+    fields->m_disableInputCutoff = geode::Mod::get()->getSettingValue<bool>("late-input-cutoff");
+    fields->m_inputOffset = static_cast<std::int32_t>(geode::Mod::get()->getSettingValue<std::int64_t>("input-offset"));
+    fields->m_inputOffsetRand = static_cast<std::int32_t>(geode::Mod::get()->getSettingValue<std::int64_t>("input-offset-rand"));
+}
+
+void CustomGJBaseGameLayer::processTimedInputs() {
+    auto& fields = m_fields;
+    auto timeMs = static_cast<std::uint64_t>(fields->m_timeOffset * 1000.0);
+    auto& commands = fields->m_timedCommands;
+    if (!commands.empty()) {
+        auto nextTime = commands.top().m_step;
+
+#if DEBUG_STEPS
+        geode::log::debug("step info: time={}, waiting for {}", timeMs, nextTime);
+#endif
+
+        while (!commands.empty() && nextTime <= timeMs) {
+            auto btn = commands.top();
+            commands.pop();
+
+#if DEBUG_STEPS
+            geode::log::debug("queuedInput: btn={} push={} p2={} timeMs={}",
+                static_cast<int>(btn.m_button), btn.m_isPush, btn.m_isPlayer2, btn.m_step);
+#endif
+
+#ifdef GEODE_IS_ANDROID
+            queueButton(static_cast<int>(btn.m_button), btn.m_isPush, btn.m_isPlayer2);
+#else
+            this->m_queuedButtons.push_back(btn);
+#endif
+
+            if (!commands.empty()) {
+                nextTime = commands.top().m_step;
+            }
+        }
+    }
+}
+
+void CustomGJBaseGameLayer::updateInputQueue() {
+    auto& fields = this->m_fields;
+    auto& commands = fields->m_timedCommands;
+    auto timeMs = static_cast<std::int32_t>(fields->m_timeOffset * 1000.0);
+    PlayerButtonCommandQueue newCommands{};
+
+    while (!commands.empty()) {
+        auto btn = commands.top();
+        commands.pop();
+        btn.m_step = std::max(btn.m_step - timeMs, 0);
+        newCommands.push(btn);
+    }
+
+    commands.swap(newCommands);
+}
+
+void CustomGJBaseGameLayer::dumpInputQueue() {
+    auto& fields = this->m_fields;
+    auto& commands = fields->m_timedCommands;
+
+    while (!commands.empty()) {
+        auto btn = commands.top();
+        commands.pop();
+
+#if DEBUG_STEPS
+        geode::log::debug("failsafe queuedInput: btn={} push={} p2={} timeMs={}",
+            static_cast<int>(btn.m_button), btn.m_isPush, btn.m_isPlayer2, btn.m_step);
+#endif
+
+#ifdef GEODE_IS_ANDROID
+        queueButton(static_cast<int>(btn.m_button), btn.m_isPush, btn.m_isPlayer2);
+#else
+        this->m_queuedButtons.push_back(btn);
+#endif
+    }
+}
+
+void CustomGJBaseGameLayer::update(float dt) {
+    auto& fields = m_fields;
+    fields->m_timeBeginMs = platform_get_time();
+    fields->m_timeOffset = 0.0;
+    fields->m_inFrame = true;
+
+    GJBaseGameLayer::update(dt);
+
+    if (fields->m_disableInputCutoff) {
+        updateInputQueue();
+    } else {
+        dumpInputQueue();
+    }
+
+    fields->m_inFrame = false;
+}
+
+void CustomGJBaseGameLayer::processCommands(float timeStep) {
+    auto timeWarp = m_gameState.m_timeWarp;
+    m_fields->m_timeOffset += timeStep / timeWarp;
+    processTimedInputs();
+    GJBaseGameLayer::processCommands(timeStep);
+}
+
+void CustomGJBaseGameLayer::fixUntimedInputs() {
+#ifdef GEODE_IS_WINDOWS
+    for (const auto& btn : this->m_queuedButtons) {
+        this->queueButton_custom(static_cast<int>(btn.m_button), btn.m_isPush, btn.m_isPlayer2);
+    }
+    this->m_queuedButtons.clear();
+#endif
 }
